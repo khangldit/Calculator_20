072ea00818f8c83fb5b4233eeb3dd8f7467cd681
Thinh Huynh <99hungthinh17@gmail.com>
2020-04-07 20:33:12 +0700

    07/04/2020

--- app/src/main/java/hcmute/edu/vn/calculator_20/MainActivity.java
+++ app/src/main/java/hcmute/edu/vn/calculator_20/MainActivity.java
@@ -14,7 +14,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button btnAdd,btnSub,btnMul,btnDiv,btnEqual;
     private Button btnPoint,btnReset;
     private TextView txtMainCal;
-    private String sign ;
+    private String sign="" ;
+    private boolean flagEquals =false;
 
     private  double val1;
     private double val2;
@@ -41,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnDiv.setOnClickListener(this);
         btnEqual.setOnClickListener(this);
         btnReset.setOnClickListener(this);
+
 
     }
     private void setupButton()
@@ -77,101 +79,168 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.btn0:
-                txtMainCal.setText(txtMainCal.getText().toString()+"0");
-                break;
-            case R.id.btn1:
-                txtMainCal.setText(txtMainCal.getText().toString()+"1");
-                break;
-            case R.id.btn2:
-                txtMainCal.setText(txtMainCal.getText().toString()+"2");
-                break;
-            case R.id.btn3:
-                txtMainCal.setText(txtMainCal.getText().toString()+"3");
-                break;
-            case R.id.btn4:
-                txtMainCal.setText(txtMainCal.getText().toString()+"4");
-                break;
-            case R.id.btn5:
-                txtMainCal.setText(txtMainCal.getText().toString()+"5");
-                break;
-            case R.id.btn6:
-                txtMainCal.setText(txtMainCal.getText().toString()+"6");
-                break;
-            case R.id.btn7:
-                txtMainCal.setText(txtMainCal.getText().toString()+"7");
-                break;
-            case R.id.btn8:
-                txtMainCal.setText(txtMainCal.getText().toString()+"8");
-                break;
-            case R.id.btn9:
-                txtMainCal.setText(txtMainCal.getText().toString()+"9");
-                break;
-            case R.id.btnPoint:
-                if(txtMainCal.getText().toString()=="")
-                    txtMainCal.setText(txtMainCal.getText().toString() +"0.");
-                else
-                    txtMainCal.setText(txtMainCal.getText().toString() +".");
-                setupButtonPoint();
-                break;
-            case R.id.btnAdd:
-                sign="+";
-                val1 = Double.parseDouble(txtMainCal.getText().toString());
-                txtMainCal.setText("");
-                setupButtonPoint();
-                break;
-            case R.id.btnSub:
-                sign="-";
-                val1 = Double.parseDouble(txtMainCal.getText().toString());
-                txtMainCal.setText("");
-                setupButtonPoint();
-                break;
-            case R.id.btnMul:
-                sign="*";
-                val1 = Double.parseDouble(txtMainCal.getText().toString());
-                txtMainCal.setText("");
-                setupButtonPoint();
-                break;
-            case R.id.btnDiv:
-                sign="/";
-                val1 = Double.parseDouble(txtMainCal.getText().toString());
-                txtMainCal.setText("");
-                setupButtonPoint();
-                break;
-            case R.id.btnEqual:
-                val2=Double.parseDouble(txtMainCal.getText().toString());
-                if (sign == "+")
-                {
-                    val1 = val1 + val2;
-                    txtMainCal.setText(String.valueOf(val1));
-                }
-                else if (sign == "-")
-                {
-                    val1 = val1 - val2;
-                    txtMainCal.setText(String.valueOf(val1));
-                }
-                else if (sign == "*")
-                {
-                    val1 = val1 * val2;
-                    txtMainCal.setText(String.valueOf(val1));
-                }
-                else
-                {
-                    if(val2==0)
-                        Toast.makeText(MainActivity.this, "Can not divide by Zero",Toast.LENGTH_SHORT).show();
-                    else {
-                        val1 = val1 / val2;
+        try{
+            switch (v.getId()){
+                case R.id.btn0:
+                    if(flagEquals)
+                        txtMainCal.setText("0");
+                    else
+                        txtMainCal.setText(txtMainCal.getText().toString() + "0");
+                    flagEquals=false;
+                    setupButtonPoint();
+                    break;
+                case R.id.btn1:
+                    if(flagEquals)
+                        txtMainCal.setText("1");
+                    else
+                        txtMainCal.setText(txtMainCal.getText().toString()+"1");
+                    flagEquals=false;
+                    setupButtonPoint();
+                    break;
+                case R.id.btn2:
+                    if(flagEquals)
+                        txtMainCal.setText("2");
+                    else
+                        txtMainCal.setText(txtMainCal.getText().toString()+"2");
+                    flagEquals=false;
+                    setupButtonPoint();
+                    break;
+                case R.id.btn3:
+                    if(flagEquals)
+                        txtMainCal.setText("3");
+                    else
+                        txtMainCal.setText(txtMainCal.getText().toString()+"3");
+                    flagEquals=false;
+                    setupButtonPoint();
+                    break;
+                case R.id.btn4:
+                    if(flagEquals)
+                        txtMainCal.setText(txtMainCal.getText().toString()+"4");
+                    else
+                        txtMainCal.setText("4");
+                    flagEquals=false;
+                    setupButtonPoint();
+                    break;
+                case R.id.btn5:
+                    if(flagEquals)
+                        txtMainCal.setText("5");
+                    else
+                        txtMainCal.setText(txtMainCal.getText().toString()+"5");
+                    flagEquals=false;
+                    setupButtonPoint();
+                    break;
+                case R.id.btn6:
+                    if(flagEquals)
+                        txtMainCal.setText("6");
+                    else
+                        txtMainCal.setText(txtMainCal.getText().toString()+"6");
+                    flagEquals=false;
+                    setupButtonPoint();
+                    break;
+                case R.id.btn7:
+                    if(flagEquals)
+                        txtMainCal.setText("7");
+                    else
+                        txtMainCal.setText(txtMainCal.getText().toString()+"7");
+                    flagEquals=false;
+                    setupButtonPoint();
+                    break;
+                case R.id.btn8:
+                    if(flagEquals)
+                        txtMainCal.setText("8");
+                    else
+                        txtMainCal.setText(txtMainCal.getText().toString()+"8");
+                    flagEquals=false;
+                    setupButtonPoint();
+                    break;
+                case R.id.btn9:
+                    if(flagEquals)
+                        txtMainCal.setText("9");
+                    else
+                        txtMainCal.setText(txtMainCal.getText().toString()+"9");
+                    flagEquals=false;
+                    setupButtonPoint();
+                    break;
+                case R.id.btnPoint:
+                    if(txtMainCal.getText().toString()=="")
+                        txtMainCal.setText(txtMainCal.getText().toString() +"0.");
+                    else
+                        txtMainCal.setText(txtMainCal.getText().toString() +".");
+                    setupButtonPoint();
+                    flagEquals=false;
+                    break;
+                case R.id.btnAdd:
+                    sign="+";
+                    val1 = Double.parseDouble(txtMainCal.getText().toString());
+                    txtMainCal.setText("");
+                    flagEquals=false;
+                    setupButtonPoint();
+                    break;
+                case R.id.btnSub:
+                    sign="-";
+                    val1 = Double.parseDouble(txtMainCal.getText().toString());
+                    txtMainCal.setText("");
+                    flagEquals=false;
+                    setupButtonPoint();
+                    break;
+                case R.id.btnMul:
+                    sign="*";
+                    val1 = Double.parseDouble(txtMainCal.getText().toString());
+                    txtMainCal.setText("");
+                    setupButtonPoint();
+                    flagEquals=false;
+                    break;
+                case R.id.btnDiv:
+                    sign="/";
+                    val1 = Double.parseDouble(txtMainCal.getText().toString());
+                    txtMainCal.setText("");
+                    flagEquals=false;
+                    setupButtonPoint();
+                    break;
+                case R.id.btnEqual:
+                    val2=Double.parseDouble(txtMainCal.getText().toString());
+                    if (sign == "+")
+                    {
+                        val1 = val1 + val2;
                         txtMainCal.setText(String.valueOf(val1));
                     }
-                }
-                break;
-            case R.id.btnReset:
-                val1=0;
-                val2=0;
-                txtMainCal.setText("");
-                setupButtonPoint();
-                break;
+                    else if (sign == "-")
+                    {
+                        val1 = val1 - val2;
+                        txtMainCal.setText(String.valueOf(val1));
+                    }
+                    else if (sign == "*")
+                    {
+                        val1 = val1 * val2;
+                        txtMainCal.setText(String.valueOf(val1));
+                    }
+                    else if(sign=="")
+                    {
+                        val1 = Double.parseDouble(txtMainCal.getText().toString());
+                    }
+                    else
+                    {
+                        if(val2==0)
+                            Toast.makeText(MainActivity.this, "Can not divide by Zero",Toast.LENGTH_SHORT).show();
+                        else {
+                            val1 = val1 / val2;
+                            txtMainCal.setText(String.valueOf(val1));
+                        }
+                    }
+                    flagEquals =true;
+                    setupButtonPoint();
+                    sign="";
+                    break;
+                case R.id.btnReset:
+                    val1=0;
+                    val2=0;
+                    sign="";
+                    txtMainCal.setText("");
+                    setupButtonPoint();
+                    break;
+            }
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
         }
     }
 }
